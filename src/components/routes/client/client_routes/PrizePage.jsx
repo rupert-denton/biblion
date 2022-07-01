be7dd064de3e6be58e5a3e041e2814a22d3d0dfd
Rupert <67182833+rupert-denton@users.noreply.github.com>
2022-07-01 22:18:06 +1200

    styling on prize page and map functions initially done

--- src/components/routes/client/client_routes/PrizePage.jsx
+++ src/components/routes/client/client_routes/PrizePage.jsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import * as api from '../../../../apiClient'
+import './PrizePage.css'
+import Navbar from '../../../ui/Navbar'
 
 export default function PrizePage() {
   let { prizeId } = useParams()
@@ -34,19 +36,63 @@ export default function PrizePage() {
       })
   }, [prizeId])
 
-  const books = bookArr.map((book, id) => {
+  const arr2021 = bookArr.filter((book) => book.year === 2021)
+  const arr2020 = bookArr.filter((book) => book.year === 2020)
+
+  const books2021 = arr2021.map((book, id) => {
     return (
-      <li key={id}>
-        <Link to={`/books/${book.book_id}`}>
-          {book.title}: {book.year}
-        </Link>
-      </li>
+      <div className="book-card" key={id}>
+        <div className="book-card-image">
+          <img
+            className="cover-image"
+            src={book.cover_image}
+            alt="The book cover"
+          />
+        </div>
+        <div className="book-title">
+          <Link to={`/books/${book.book_id}`}>
+            {book.title}: {book.year}
+          </Link>
+        </div>
+        <div className="author-name">{book.name}</div>
+      </div>
+    )
+  })
+
+  const books2020 = arr2020.map((book, id) => {
+    return (
+      <div className="book-card" key={id}>
+        <div className="book-card-image">
+          <img
+            className="cover-image"
+            src={book.cover_image}
+            alt="The book cover"
+          />
+        </div>
+        <div className="book-title">
+          <Link to={`/books/${book.book_id}`}>
+            {book.title}: {book.year}
+          </Link>
+        </div>
+        <div className="author-name">{book.name}</div>
+      </div>
     )
   })
 
   return (
-    <div>
-      <div>{books}</div>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <div className="prize-page">
+        <div className="prize-header">{prizeInfo.prize_name}</div>
+        <div className="prize-winners-group">
+          <div className="year">2021</div>
+          <div className="book-card-container">{books2021}</div>
+        </div>
+        <div className="prize-winners-group">
+          <div className="year">2020</div>
+          <div className="book-card-container">{books2020}</div>
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
